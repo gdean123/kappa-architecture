be1b4f03afc560a698f14a5dd42e3096d8cbd4b7
George Dean <gdean@pivotal.io>
2018-02-27 16:33:56 -0800

    Create 'kappa connectors load|unload'

---
+++ operation/src/main/kotlin/operation/domain/ManageConnectors.kt
@@ -0,0 +1,20 @@
+package operation.domain
+
+import operation.support.Log
+import operation.tools.Confluent
+
+object ManageConnectors {
+    fun load() {
+        Confluent.load("jdbc-sink", "connect/postgres-sink.properties")
+    }
+
+    fun unload() {
+        Confluent.unload("jdbc-sink")
+    }
+
+    fun reload() {
+        unload()
+        Log.newline()
+        load()
+    }
+}
--- operation/src/main/kotlin/operation/user_interface/Connectors.kt
+++ operation/src/main/kotlin/operation/user_interface/Connectors.kt
@@ -1,18 +1,30 @@
 package operation.user_interface
 
-import operation.domain.ReloadAllConnectors
+import operation.domain.ManageConnectors
 import picocli.CommandLine.Command
 
 @Command(
     name = "connectors",
     description = ["Manage Kafka Connect connectors"],
     subcommands = [
+        Connectors.Load::class,
+        Connectors.Unload::class,
         Connectors.Reload::class
     ]
 )
 class Connectors : Group() {
-    @Command(name = "reload", description = ["Load all connectors"])
+    @Command(name = "load", description = ["Load all connectors"])
+    class Load: Runnable {
+        override fun run() = ManageConnectors.load()
+    }
+
+    @Command(name = "unload", description = ["Unload all connectors"])
+    class Unload: Runnable {
+        override fun run() = ManageConnectors.unload()
+    }
+
+    @Command(name = "reload", description = ["Unload and reload all connectors"])
     class Reload: Runnable {
-        override fun run() = ReloadAllConnectors.execute()
+        override fun run() = ManageConnectors.reload()
     }
 }
