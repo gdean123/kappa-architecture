7394f774322dbf9a5294a9ed929dc1bc32562d03
George Dean <gdean@pivotal.io>
2018-03-15 17:46:47 -0700

    Create 'kappa test stream-processor'

--- operation/src/main/kotlin/operation/domain/actions/RunTests.kt
+++ operation/src/main/kotlin/operation/domain/actions/RunTests.kt
@@ -7,4 +7,5 @@ import operation.tools.Gradle
 object RunTests {
     fun producer() = Gradle.test(Paths.producer(), Environment.read("producer", "test", "application"))
     fun consumer() = Gradle.test(Paths.consumer(), Environment.read("consumer", "test", "application"))
+    fun streamProcessor() = Gradle.test(Paths.streamProcessor(), Environment.read("stream-processor", "test", "application"))
 }
--- operation/src/main/kotlin/operation/user_interface/Test.kt
+++ operation/src/main/kotlin/operation/user_interface/Test.kt
@@ -7,12 +7,17 @@ import picocli.CommandLine
 @CommandLine.Command(
     name = "test",
     description = ["Run tests"],
-    subcommands = [Test.Producer::class, Test.Consumer::class]
+    subcommands = [Test.Producer::class, Test.StreamProcessor::class, Test.Consumer::class]
 )
 class Test : Group() {
     @CommandLine.Command(name = "producer", description = ["Run the producer tests"])
     class Producer: Runnable {
         override fun run() = RunTests.producer()
+    }
+
+    @CommandLine.Command(name = "stream-processor", description = ["Run the stream-processor tests"])
+    class StreamProcessor: Runnable {
+        override fun run() = RunTests.streamProcessor()
     }
 
     @CommandLine.Command(name = "consumer", description = ["Run the consumer tests"])
